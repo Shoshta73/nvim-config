225ba117bbbeac75fc63c7dc2de6b135e91acd3c
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-06-25 21:21:35 +0200

    update

--- init.lua
+++ init.lua
@@ -1,1 +1,4 @@
+if vim.g.neovide then
+	vim.o.guifont = "Mononoki Nerd Font Mono:h14"
+end
 require("user")
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -40,7 +40,8 @@ return {
 
 			require("lualine").setup({
 				options = {
-					theme = "catppuccin",
+					-- theme = "catppuccin",
+					theme = "vscode",
 					globalstatus = true,
 					component_separators = { left = "", right = "" },
 					section_separators = { left = "█", right = "█" },
